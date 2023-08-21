cad82f9f41702d61c4ae09f0e9625962a5891975
israt <isratjahanpanna87@gmail.com>
2023-08-21 13:05:25 +0600

    total calculation done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-
+let total = 0;
 function handelClickBtn(target) {
     const selectedItemsContainer = document.getElementById('selectedItems');
     const itemName = target.querySelector('.card-body p').innerText; 
     const li = document.createElement('li');
     li.innerText = itemName;
     selectedItemsContainer.appendChild(li);
-    console.log (target.querySelector('.card-body h4').innerText.split(" ") [0] );
-    
+    const price = target.querySelector('.card-body h4').innerText.split(" ") [0] ;
+    total = parseInt(total) + parseInt(price );
+    const totalPrice = document.getElementById('totalPrice').innerText = total;
   }
  
   
