1d84227fa9fba2818aa69f4034d2313f6f7529ea
israt <isratjahanpanna87@gmail.com>
2023-08-21 13:52:49 +0600

    total and purchase enable

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 let total = 0;
+let discount = 0;
+// let finalTotal = 0;
+
 function handelClickBtn(target) {
     const selectedItemsContainer = document.getElementById('selectedItems');
     const itemName = target.querySelector('.card-body p').innerText; 
@@ -8,6 +11,28 @@ function handelClickBtn(target) {
     const price = target.querySelector('.card-body h4').innerText.split(" ") [0] ;
     total = parseInt(total) + parseInt(price );
     const totalPrice = document.getElementById('totalPrice').innerText = total;
+    if (totalPrice >= 200){
+      discount = 0.20 * totalPrice;
+      const discountAmount = document.getElementById('discount').innerText = discount;
+    }
+    else {
+      discount = 0;
+      document.getElementById('discount').innerText = discount.toFixed(2);
+    }
+    const finalPrice = total - discount;
+    document.getElementById('finalPrice').innerText = finalPrice.toFixed(2);
+    document.getElementById('purchase-btn').addEventListener('keyup', function(event) {
+      const text = event.target.value;
+      const purchaseButton = document.getElementById('purchase-btn');
+      
+      if (total >= 1) {
+        purchaseButton.removeAttribute('disabled');
+      } else {
+        purchaseButton.setAttribute('disabled', true);
+      }
+      
+    });
   }
+  
  
   
